c1757045bb7dd131406bd55cf63976dcbee29a62
Ed Wheeler <ewheeler3@gmail.com>
2013-05-26 12:56:03 -0400

    initial commit

---
+++ js/songdata.js
@@ -0,0 +1,12 @@
+var songdata = [
+	{
+		"number" : "1",
+		"scripture" : "(Revelation 4:11)",
+		"verse" : [			
+				"Jehovah God, exalted in might, Source of all life and Provider of light.<br /> Creation speaks of your power so great; More your great day will still relate.",
+				"Your lofty throne, on justice it stands. Thus you make known all your righteous commands. And as we turn to your Word, we can see Your wisdom shine so brilliantly.", 
+				"Greatest of all is your perfect love. None can repay all your gifts from above. Your attributes and your glorious name, Exultingly we will proclaim."	
+		],
+		"footer" : "(See also Ps. 36:9; 145:6-13; Jas. 1:17.)"	
+	}
+];
