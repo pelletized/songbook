e1c8b3d81dd5028a6a6d654acc5fc95b286c53f8
Ed Wheeler <ew59@nyu.edu>
2013-06-10 20:03:21 -0400

    added song data, worked on song layout

--- js/songdata.js
+++ js/songdata.js
@@ -1,12 +1,50 @@
 var songdata = [
 	{
 		"number" : "1",
+		"title" : "Jehovah's Attributes",
 		"scripture" : "(Revelation 4:11)",
 		"verse" : [			
-				"Jehovah God, exalted in might, Source of all life and Provider of light.<br /> Creation speaks of your power so great; More your great day will still relate.",
-				"Your lofty throne, on justice it stands. Thus you make known all your righteous commands. And as we turn to your Word, we can see Your wisdom shine so brilliantly.", 
-				"Greatest of all is your perfect love. None can repay all your gifts from above. Your attributes and your glorious name, Exultingly we will proclaim."	
+				"1. Jehovah God, exalted in might, Source of all life and Provider of light.<br /> Creation speaks of your power so great; More your great day will still relate.",
+				
+				"2. Your lofty throne, on justice it stands. Thus you make known all your righteous commands. And as we turn to your Word, we can see Your wisdom shine so brilliantly.", 
+				
+				"3. Greatest of all is your perfect love. None can repay all your gifts from above. Your attributes and your glorious name, Exultingly we will proclaim."	
 		],
+		//"chorus" : "this is a chorus",
 		"footer" : "(See also Ps. 36:9; 145:6-13; Jas. 1:17.)"	
+	}, 	
+	{
+		"number" : "2",
+		"title" : "We Thank You, Jehovah",
+		"scripture" : "(1 Thessalonians 5:18)",
+		"verse" : [			
+				"1. We thank you, Jehovah, each day and each night, That you shed upon us your precious light. We thank you that we have the priv'lege of prayer, That we can approach you with ev'ry care.",
+				
+				"2. We thank you, Jehovah, for your loving Son, Who conquered the world; By his faith he won. We thank you for guidance in doing your will, For thus you do help us our vows fulfill. ", 
+				
+				"3. We thank you, our God, for the honor to preach About your great name and the truth to teach. We thank you that soon all earth's woes will be past, While your Kingdom blessings forever last."
+		],
+		//"chorus" : "this is a chorus",
+		"footer" : "(See also Ps. 50:14; 95:2; 147:7; Col. 3:15.)"	
+	},
+	{
+		"number" : "3",
+		"title" : "God Is Love",
+		"scripture" : "(1 John 4:7, 8)",
+		"verse" : [			
+				"1. God is love, and he invites us, ‘Walk with me and love my way.' When we love both God and neighbor, Gracious deeds will fill each day. Therein lies the key to living; Therein lies the life we seek. Christlike love will never fail us. Christlike love through us will speak.",
+
+				"2. Love of truth moves us to action; Love of God moves us to love. When we fail he seeks to help us; By his strength we rise above. Love is pure and never jealous; Love is kind and bears all things. May we grow to love our brother. May we taste what true love brings. ",
+
+				"3. Never let resentment lead you; May it never take your hand. Look to God, and he will guide you; He will teach you these commands: Love of God and love of neighbor, Ev'rything that love should be. May we always show to others Godlike love, yes, tenderly."
+		],
+		//"chorus" : "this is a chorus",
+		"footer" : "(See also Mark 12:30, 31; 1 Cor. 12:31–13:8; 1 John 3:23.)"	
 	}
+	
+	
 ];
+
+/*
+TODO: start with 4
+*/
